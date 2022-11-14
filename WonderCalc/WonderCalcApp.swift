14870dbe2522ea7dbbfac47bf383baa0fb6f7712
Steven Schwedt <sschwedt@gmail.com>
2022-11-14 09:59:45 -0700

    Initial Commit

---
+++ WonderCalc/WonderCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  WonderCalcApp.swift
+//  WonderCalc
+//
+//  Created by Steven Schwedt on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct WonderCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
