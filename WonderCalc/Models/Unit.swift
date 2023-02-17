f5dc054b6a0e27da95604afa76a5133dd8100d93
Steven Schwedt <sschwedt@gmail.com>
2023-02-17 11:08:23 -0700

    Adds area units

--- WonderCalc/Models/Unit.swift
+++ WonderCalc/Models/Unit.swift
@@ -45,6 +45,7 @@ class UnitProvider: ObservableObject {
 }
 
 enum UnitType: String, Pickable, CaseIterable {
+    case area
     case length
     case temperature
     case time
@@ -67,8 +68,43 @@ enum UnitType: String, Pickable, CaseIterable {
             return WeightUnit.self
         case .volume:
             return VolumeUnit.self
+        case .area:
+            return AreaUnit.self
         }
     }
+}
+
+enum AreaUnit: String, Unitable {
+    case squareMeters = "square meters" // standard
+    case squareKilometers = "square kilometers"
+    case squareInches = "square inches"
+    case squareFeet = "square feet"
+    case squareMiles = "square miles"
+    case acres
+
+    var multiplier: Double {
+        switch self {
+        case .squareMeters:
+            return 1
+        case .squareKilometers:
+            return 1 / 1000000
+        case .squareInches:
+            return sqMeterToInch
+        case .squareFeet:
+            return sqMeterToInch / 144
+        case .squareMiles:
+            return sqMeterToInch / pow(5280 * 12, 2)
+        case .acres:
+            return sqMeterToInch / (pow(5280 * 12, 2) / 640)
+        }
+    }
+
+    var adder: Double { 0.0 }
+
+    static var firstOption: AreaUnit { .squareMeters }
+    static var secondOption: AreaUnit { .squareFeet }
+
+    private var sqMeterToInch: Double { 1550.0031 }
 }
 
 enum TimeUnit: String, Unitable {
@@ -95,6 +131,7 @@ enum TimeUnit: String, Unitable {
         case .years:
             return 1 / (60 * 60 * 24 * 7 * 365)
         }
+
     }
 
     var adder: Double { 0 }
