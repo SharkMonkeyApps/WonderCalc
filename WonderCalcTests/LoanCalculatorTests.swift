81f871012d748f153ef0ad4203452dc38f7e0481
Steven Schwedt <sschwedt@gmail.com>
2023-03-05 11:52:25 -0700

    Tests loan calculation for amount

--- WonderCalcTests/LoanCalculatorTests.swift
+++ WonderCalcTests/LoanCalculatorTests.swift
@@ -16,12 +16,22 @@ final class LoanCalculatorTests: XCTestCase {
         loanCalculator = LoanCalculator()
     }
     
-    func test_itCanCalculateALoan() {
+    func test_itCanCalculateAPayment() {
         loanCalculator.amount = "100000"
         loanCalculator.years = "10"
         loanCalculator.rate = "6"
         
-        loanCalculator.payments.monthly = "$1,110.21"
-        loanCalculator.payments.total = "$133,224.60"
+        XCTAssertEqual(loanCalculator.payments.monthly, "$1,110.21")
+        XCTAssertEqual(loanCalculator.payments.total, "$133,224.60")
+        XCTAssertEqual(loanCalculator.payments.interest, "$33,224.60")
+    }
+
+    func test_itCanCalculateAnAmount() {
+        loanCalculator.amountToPayment = false
+        loanCalculator.monthlyPayment = "2000"
+        loanCalculator.years = "30"
+        loanCalculator.rate = "10"
+
+        XCTAssertEqual(loanCalculator.amountResult, "$227,901.64")
     }
 }
